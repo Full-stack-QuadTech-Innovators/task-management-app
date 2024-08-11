7c75be8b22d0cf31b276d0301990558f23a002f9
BenstR <66129129+benstr1702@users.noreply.github.com>
2024-08-11 09:57:56 +0300

    temporary auth to redirect to login

---
+++ client/src/util/protected-routes/UserProtectedRoutes.jsx
@@ -0,0 +1,16 @@
+import { Outlet, Navigate } from "react-router-dom";
+// import UserContext from "../../contexts/userContext";
+import { useContext } from "react";
+
+export default function UserProtectedRoutes() {
+	//TBD: Add authentication for users.
+	// const { users, loggedIn } = useContext(UserContext);
+
+	// const isLoggedIn = loggedIn && Object.keys(loggedIn).length > 0;
+	// const currentUser = isLoggedIn
+	// 	? users.find((user) => user.uuid === loggedIn.uuid)
+	// 	: null;
+	const currentUser = null;
+
+	return currentUser ? <Outlet /> : <Navigate to="/login/" />;
+}
