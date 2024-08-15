2a6d9ad0bc66325ffd3816d9544775d5b1f8c96e
BenstR <66129129+benstr1702@users.noreply.github.com>
2024-08-16 02:15:12 +0300

    db fix

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-
+console.log("MONGODB_URI:", process.env.MONGODB_URI);
 // if (!process.env.MONGODB_URI) {
 // 	console.error("MONGODB_URI environment variable is not defined.");
 // 	process.exit(1);
